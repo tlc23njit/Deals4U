ed5fd6e4a033e75e87244ab08669b0e2d6627081
tlc23njit <tlc23@njit.edu>
2024-02-07 20:19:19 -0500

    finished ebay webscraping without category

--- frontend/ebayscrape.py
+++ frontend/ebayscrape.py
@@ -12,25 +12,42 @@ for i in websites:
     #results = soup.find_all(itemprop="price")
     results = soup.find_all("div", class_ = "dne-itemtile dne-itemtile-medium")
     results += soup.find_all("div", class_ = "dne-itemtile dne-itemtile-large")
-    print(len(soup.find_all("div", class_ ="dne-itemtile-detail")))
+    #print(len(soup.find_all("div", class_ ="dne-itemtile-detail")))
     for i in range(len(results)):
         results[i]=results[i].prettify()
-        d[i]=[]
+        d[i]={}
         n=""
-        for j in results[i].split():
-            if j[0:5] == "href=":
-                d[i].append(j[6:-1])
-            if j[0:5] == "title":
+        k=results[i].split()
+        ub = True
+        ib = True
+        for j in range(len(k)):
+            if k[j][0:5] == "href=" and ub:
+                ub = False
+                d[i]["url"]=k[j][6:-1]
+            if k[j][0:5] == "title":
                 nb = True
             if nb:
-                if j[-2:]=='">':
+                n+=k[j]+" "
+                if k[j][-2:]=='">' or k[j][-2:]=="'>":
                     nb = False
-                    d[i].append(n[7:-1])
-                n+=j+" "
+                    d[i]["name"]=n[7:-3]
+            if k[j][0:15]=="data-config-src" and ib:
+                d[i]["img"]=k[j][17:-1]
+                ib = False
+            if k[j][0:3]=="src" and ib:
+                d[i]["img"]=(k[j][4:-3])
+                ib = False
+            if k[j]=='itemprop="price">':
+                d[i]["price"]=(k[j+1])
+            if k[j]=="Previous":
+                d[i]["discount"]=k[j+3]
 #for i in d:
     #print(d[i])
 #print(results[0].split())
 #print(d[0])
-print(len(d))
-print(len(results))
-print(d[0])
+#print(len(d))
+#print(len(results))
+#print(d[0])
+#print(d[1])
+#print(results[-1].split())
+#print(d[len(d)-1])
